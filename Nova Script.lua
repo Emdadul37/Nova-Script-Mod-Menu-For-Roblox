060e6a9ea393e8729c25ff40a848371ddd18fd58
Emdadul37 <gamer.emdadul01@gmail.com>
2026-03-14 16:34:15 +0600

    Update script URL in Nova Script.lua

--- Nova Script.lua
+++ Nova Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/3b3f36fb18624b4e"))()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/ce4dfb10def14cf0"))()
